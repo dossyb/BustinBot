1b902815cdb3a0f560ed5adcec0493d163b8c841
dossyx <hboyd98@outlook.com>
2024-09-28 20:49:34 +1000

    Add and list movies

---
+++ index.js
@@ -0,0 +1,64 @@
+const { Client, GatewayIntentBits } = require('discord.js');
+const fs = require('fs');
+const path = './movies.json';
+const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent] });
+
+let movieList = [];
+
+// Load movies from JSON file
+function loadMovies() {
+    if (fs.existsSync(path)) {
+        movieList = JSON.parse(fs.readFileSync(path, 'utf8'));
+    }
+}
+
+// Save movies to JSON file
+function saveMovies() {
+    fs.writeFileSync(path, JSON.stringify(movieList, null, 4), 'utf8');
+}
+
+client.once('ready', () => {
+    console.log('BustinBot is online!');
+    loadMovies();
+});
+
+client.on('messageCreate', (message) => {
+    if (!message.content.startsWith('!')) return;
+
+    const args = message.content.slice(1).trim().split(/ +/);
+    const command = args.shift().toLowerCase();
+
+    if (command === '!bustin') {
+        message.channel.send('Bustin\' makes me feel good!');
+    }
+
+    if (command === 'addmovie') {
+        const movieName = args.join(' ');
+
+        if (!movieName) {
+            message.channel.send('Please provide a movie name.');
+            return;
+        }
+
+        const movieObject = {
+            name: movieName,
+            suggestedby: message.author.tag
+        };
+
+        movieList.push(movieObject);
+        saveMovies();
+        message.channel.send(`Added ${movieName} to the movie list.`);
+    }
+
+    if (command === 'movielist') {
+        if (movieList.length === 0) {
+            message.channel.send('The movie list is empty.');
+            return;
+        } else {
+            const movieDescriptions = movieList.map(movie => `${movie.name} - suggested by: ${movie.suggestedby}`);
+            message.channel.send(`Movies in the list:\n${movieDescriptions.join('\n')}`);
+        }
+    }
+});
+
+client.login('MTI4OTUxNjUxNDcxMzc5NjY5Mg.G4C0zj.DotDXhEw7U2aoPnUPMUQnasAgJRqw-wAkBoasU');
