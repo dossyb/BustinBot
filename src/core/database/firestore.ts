8d25f82c152b1776691747bf822d91f5d558ff48
dossyb <hboyd98@outlook.com>
2025-11-01 21:38:22 +1100

    fix(core): fix Firestore credential handling

--- src/core/database/firestore.ts
+++ src/core/database/firestore.ts
@@ -1,14 +1,16 @@
 import admin from 'firebase-admin';
 import { getFirestore } from 'firebase-admin/firestore';
-import path from 'path';
-import { getDirname } from '../../utils/PathUtils.js';
-const dirname = getDirname(import.meta.url);
 
-const serviceAccountPath = path.join(dirname, '../../config/bustinbot-3840f-firebase-adminsdk-fbsvc-39e719dd30.json');
+const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_KEY!);
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+});
+
 
 if (!admin.apps.length) {
     admin.initializeApp({
-        credential: admin.credential.cert(serviceAccountPath),
+        credential: admin.credential.cert(serviceAccount),
     });
 }
 
